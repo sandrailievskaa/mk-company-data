2d8420aab090efe320bcddddea81cdc19a9110a9
Sandra Ilievska <silievska85@yahoo.com>
2025-12-17 19:12:13 +0100

    Install OpenAi package and configure an AI bot into the application. #2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test-openai', function () {
+    $response = OpenAI::responses()->create([
+        'model' => 'gpt-5',
+        'input' => 'Hello!',
+    ]);
+
+    return $response->outputText;
+});
+
