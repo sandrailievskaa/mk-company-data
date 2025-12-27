255029a518dddd63165ae530eadd6545b035fec2
sandrailievskaa <131893105+sandrailievskaa@users.noreply.github.com>
2025-12-27 18:19:31 +0100

    Install OpenAI Laravel package (#5)
    * Install OpenAI Laravel package
    
    * Update OpenAI model from gpt-5 to gpt-4o-mini for testing
    
    * fix

--- app/Filament/Resources/Companies/Pages/ListCompanies.php
+++ app/Filament/Resources/Companies/Pages/ListCompanies.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\Companies\Pages;
 
 use App\Filament\Resources\Companies\CompanyResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCompanies extends ListRecords
